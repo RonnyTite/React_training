78f5fae34cad830651ac8929689083ffa24009e4
Ronny Tite <ronny.tite@m-itrust.com>
2023-08-10 18:30:44 +0200

    isntall project

---
+++ src/images/images.js
@@ -0,0 +1,17 @@
+import dirtImg from './dirt.jpg';
+import grassImg from './grass.jpg';
+import glassImg from './glass.png';
+import logImg from './log.jpg';
+import woodImg from './wood.png';
+import logoImg from './Minecraft_Logo.png';
+
+const images = {
+    dirtImg,
+    grassImg,
+    glassImg,
+    logImg,
+    woodImg,
+    logoImg,
+}
+
+export default images;
