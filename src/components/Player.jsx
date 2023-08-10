cb1d0672541321658ad6b7407663afc600c76923
Ronny Tite <ronny.tite@m-itrust.com>
2023-08-10 21:50:14 +0200

    keyboardInputs OK

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,15 +2,20 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { Vector3 } from "three";
 import { useEffect, useRef } from "react";
+import useKeyboard from "../hooks/useKeyboard";
 const Player = () => {
+
+    const actions = useKeyboard();
+    console.log('actions', Object.entries(actions).filter(([k, v]) => v));
     const { camera } = useThree();
+    
     const [ref, api] = useSphere(() => ({
         mass: 1,
         type: 'Dynamic',
         position: [0, 1, 0],
     }));
 
-// velocity
+    // velocity
     const vel = useRef([0, 0, 0,]);
     useEffect(() => {
         api.velocity.subscribe((v) => { vel.current = v })
@@ -25,8 +30,7 @@ const Player = () => {
     // call every frame
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
-
-        api.velocity.set(0,1,0);
+        api.velocity.set(0, 0, 0);
     });
 
     return (
