5167e7674a5b0c5ac5e8053594c5f7ac915cdd9b
Ronny Tite <ronny.tite@m-itrust.com>
2023-08-10 19:27:04 +0200

    Ground

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import Ground from './components/ground.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ground.jsx
@@ -0,0 +1,23 @@
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from "../images/textures";
+import { NearestFilter, RepeatWrapping } from "three";
+const Ground = () => {
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 4, 0, 0],
+        position: [0, 0, 0]
+    }));
+
+    groundTexture.magFilter = NearestFilter;
+    groundTexture.wrapS = RepeatWrapping;
+    groundTexture.wrapT = RepeatWrapping;
+    groundTexture.repeat.set(100, 100);
+
+    return (
+        <mesh ref={ref}>
+            <planeGeometry args={[100, 100]} /> // BufferplaneGeometry was deprectaded https://stackoverflow.com/a/76792805
+            <meshStandardMaterial map={groundTexture} />
+        </mesh>
+    )
+
+};
+export default Ground;
