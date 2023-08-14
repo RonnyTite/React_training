97ad84d79a06fecfdcec53bf23bfff68c78536bf
Ronny Tite <ronny.tite@m-itrust.com>
2023-08-14 15:32:49 +0200

    first person view is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import Ground from './components/Ground.jsx';
 import Player from './components/Player.jsx';
+import FPV from './components/FirstPersonView.jsx';
 
 function App() {
 
@@ -11,11 +12,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className='absolute centered cursor'>+</div>
     </>
   )
 }
